88ee49ae3cd872e9d248d59b771fe2bd2f71a503
ismayilrhmv <ismayilhr-ap120@code.edu.az>
2025-03-06 19:49:53 +0400

    first comit

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Console.Write("eded daxil edin--");
+//int n=int.Parse(Console.ReadLine());
+//int sum = 0;
+
+//while (n > 0)
+//{
+//    sum += n % 10;
+//    n /= 10;
+//}
+
+//Console.WriteLine("Reqemlerin cemi: " + sum);
+
+
+Console.Write("eded daxil edin--");
+int num = int.Parse(Console.ReadLine());
+string poli = num.ToString();
+string polinom = "";
+while (num > 0)
+{
+    polinom += num % 10;
+    num /= 10;
+}
+
+if(polinom ==  poli)
+{
+    Console.WriteLine("polinomdur");
+}
+else
+{
+    Console.WriteLine("polinom deyil");
+}
+
